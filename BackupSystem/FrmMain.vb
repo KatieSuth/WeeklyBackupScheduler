177e3aa4c04e8713d2df6e68206b1d33a95dee59
Katie Sutherland <katiesuth4@gmail.com>
2018-02-11 23:15:29 -0500

    Added Files

---
+++ BackupSystem/FrmMain.vb
@@ -0,0 +1,261 @@
+﻿Public Class frmMain
+
+#Region "Variables Declarations"
+    Dim dtNextBackup As New DateTime
+    Dim strSettingsPath As String = System.IO.Path.Combine(System.Environment.SpecialFolder.ApplicationData, "WeeklyBackupScheduler", "settings.txt")
+#End Region
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ReadSettings()
+    End Sub
+
+    Private Sub frmMain_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+        If Me.WindowState = FormWindowState.Minimized Then
+            icoNotify.Visible = True
+            icoNotify.Icon = SystemIcons.Application
+            icoNotify.BalloonTipIcon = ToolTipIcon.Info
+            icoNotify.BalloonTipTitle = "Weekly Backup Scheduler"
+            icoNotify.BalloonTipText = "Backup scheduled for: " & dtNextBackup.ToShortDateString()
+            ShowInTaskbar = False
+        End If
+    End Sub
+
+    Private Sub icoNotify_BalloonTipShown(sender As Object, e As EventArgs) Handles icoNotify.BalloonTipShown
+        icoNotify.BalloonTipText = "Backup scheduled for: " & dtNextBackup.ToShortDateString()
+    End Sub
+
+    Private Sub icoNotify_Click(sender As Object, e As EventArgs) Handles icoNotify.Click
+        Me.Show()
+        ShowInTaskbar = True
+        Me.WindowState = FormWindowState.Normal
+        icoNotify.Visible = False
+    End Sub
+
+#Region "Button Methods"
+    Private Sub cmdBrowseLoc_Click(sender As Object, e As EventArgs) Handles cmdBrowseLoc.Click
+        Try
+            If fdbGetFolder.ShowDialog() = DialogResult.OK Then
+                txtLocation.Text = fdbGetFolder.SelectedPath
+            End If
+        Catch ex As Exception
+            ShowErrorMessage(ex)
+        End Try
+    End Sub
+
+    Private Sub cmdBrowseCopy_Click(sender As Object, e As EventArgs) Handles cmdBrowseCopy.Click
+        Try
+            If fdbGetFolder.ShowDialog() = DialogResult.OK Then
+                lstPathsToCopy.Items.Add(fdbGetFolder.SelectedPath)
+            End If
+        Catch ex As Exception
+            ShowErrorMessage(ex)
+        End Try
+    End Sub
+
+    Private Sub cmdRemove_Click(sender As Object, e As EventArgs) Handles cmdRemove.Click
+        Try
+            If lstPathsToCopy.SelectedItems.Count > 0 Then
+                lstPathsToCopy.Items.Remove(lstPathsToCopy.SelectedItem)
+            End If
+        Catch ex As Exception
+            ShowErrorMessage(ex)
+        End Try
+    End Sub
+
+    Private Sub cmdSchedule_Click(sender As Object, e As EventArgs) Handles cmdSchedule.Click
+        Try
+            If txtLocation.Text.Trim() <> "" And lstPathsToCopy.Items.Count > 0 Then
+                If ConfirmSchedule() = True Then
+                    Me.WindowState = FormWindowState.Minimized
+                    Me.Hide()
+                    WriteSettings()
+                    Backup()
+                End If
+            Else
+                MessageBox.Show("One or more of the fields has not been completed. Please make sure you've entered the path " &
+                                "where you want your backup to go to ('Backup Location') and the paths to the files you want " &
+                                "to copy.", "Missing Information",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        Catch ex As Exception
+            ShowErrorMessage(ex)
+        End Try
+    End Sub
+#End Region
+
+#Region "Methods for Exceptions"
+    Private Sub ShowErrorMessage(ByVal ex As Exception)
+        Dim Trace As New System.Diagnostics.StackTrace(ex, True)
+        Dim strMessage As String = ex.Message.ToString()
+        Dim strFile As String = Trace.GetFrame(0).GetFileName()
+        Dim strLine As String = Trace.GetFrame(0).GetFileLineNumber().ToString()
+
+        MessageBox.Show("Error in file: " & strFile & " on line " & strLine & "." &
+                        vbNewLine & vbNewLine & "Error Message:" & vbNewLine & strMessage,
+                        "Error: Backup System", MessageBoxButtons.OK, MessageBoxIcon.Error)
+    End Sub
+#End Region
+
+#Region "Selection Changed"
+    Private Sub lstPathsToCopy_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstPathsToCopy.SelectedIndexChanged
+        Try
+            If lstPathsToCopy.Items.Count > 0 Then
+                cmdRemove.Enabled = True
+            Else
+                cmdRemove.Enabled = False
+            End If
+        Catch ex As Exception
+            ShowErrorMessage(ex)
+        End Try
+    End Sub
+
+    Private Sub numMaxCopies_ValueChanged(sender As Object, e As EventArgs) Handles numMaxCopies.ValueChanged
+        Try
+            If numMaxCopies.Value > 1 Then
+                lblCopies.Text = "copies"
+            Else
+                lblCopies.Text = "copy"
+            End If
+        Catch ex As Exception
+            ShowErrorMessage(ex)
+        End Try
+    End Sub
+#End Region
+
+#Region "Utility"
+    Private Function ConfirmSchedule() As Boolean
+        Try
+            'Start with confirmation message
+            Dim strMessage As String = "With these settings, all files specified in the paths listed will be copied to " & txtLocation.Text &
+                                       " every week at " & dtpTime.Value.ToShortTimeString & ". Up to " & numMaxCopies.Value
+
+            If numMaxCopies.Value > 1 Then
+                strMessage = strMessage & " copies"
+            Else
+                strMessage = strMessage & " copy"
+            End If
+
+            strMessage = strMessage & " of your files will be kept as a backup at a time. Your first backup is scheduled to begin "
+
+            If cbBackupNow.Checked = True Then
+                strMessage = strMessage & "now. "
+                dtNextBackup = New DateTime(Date.Now.Year, Date.Now.Month, Date.Now.Day,
+                                            dtpTime.Value.Hour, dtpTime.Value.Minute, 0)
+            Else
+                Dim intDays As Integer = (Convert.ToInt32(DayOfWeek.Sunday) - Convert.ToInt32(Date.Now.DayOfWeek) + 7) Mod 7
+                dtNextBackup = New DateTime(Date.Now.AddDays(intDays).Year, Date.Now.AddDays(intDays).Month, Date.Now.AddDays(intDays).Day,
+                                            dtpTime.Value.Hour, dtpTime.Value.Minute, 0)
+
+                strMessage = strMessage & "on " & dtNextBackup.Date.ToShortDateString()
+            End If
+
+            strMessage = strMessage & vbNewLine & vbNewLine & "Confirm?"
+
+            If MessageBox.Show(strMessage, "Confirm Schedule", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes Then
+                Return True
+            Else
+                Return False
+            End If
+        Catch ex As Exception
+            ShowErrorMessage(ex)
+            Return False
+        End Try
+    End Function
+
+    Private Sub Backup()
+        Try
+            While (True)
+                If Date.Now >= dtNextBackup Then 'loop until we're at the next backup date
+                    Dim diBackupLocation As New IO.DirectoryInfo(txtLocation.Text)
+                    Dim listBackups As New List(Of IO.DirectoryInfo)
+
+                    For Each folder As IO.DirectoryInfo In diBackupLocation.GetDirectories()
+                        If folder.Name.StartsWith("BackupWBS_") Then
+                            listBackups.Add(folder)
+                        End If
+                    Next
+
+                    If listBackups.Count >= numMaxCopies.Value Then
+                        Dim intOldestIndex As Integer = 0
+
+                        For counter As Integer = 1 To listBackups.Count - 1
+                            If listBackups(intOldestIndex).CreationTime > listBackups(counter).CreationTime Then
+                                intOldestIndex = counter
+                            End If
+                        Next
+
+                        System.IO.Directory.Delete(listBackups(intOldestIndex).FullName, True)
+                    End If
+
+                    Dim strNewFile As String = IO.Path.Combine(txtLocation.Text, "BackupWBS_" & Date.Now.Year & "-" & Date.Now.Month & "-" & Date.Now.Day)
+
+                    System.IO.Directory.CreateDirectory(strNewFile)
+
+                    For Each path As String In lstPathsToCopy.Items
+                        My.Computer.FileSystem.CopyDirectory(path, strNewFile)
+                    Next
+
+                    Dim intDays As Integer = (Convert.ToInt32(DayOfWeek.Sunday) - Convert.ToInt32(Date.Now.DayOfWeek) + 7) Mod 7
+                    If intDays = 0 Then
+                        intDays = 7
+                    End If
+                    dtNextBackup = New DateTime(Date.Now.AddDays(intDays).Year, Date.Now.AddDays(intDays).Month, Date.Now.AddDays(intDays).Day,
+                                                dtpTime.Value.Hour, dtpTime.Value.Minute, 0)
+                End If
+            End While
+        Catch ex As Exception
+            ShowErrorMessage(ex)
+        End Try
+    End Sub
+
+    Private Sub ReadSettings()
+        Try
+            Dim intCounter As Integer = 0
+
+            If IO.File.Exists(strSettingsPath) Then
+                Using fileReader As IO.StreamReader = New IO.StreamReader(strSettingsPath)
+                    While Not fileReader.EndOfStream
+                        Select Case intCounter
+                            Case 0
+                                txtLocation.Text = fileReader.ReadLine()
+                            Case 1
+                                dtpTime.Value = Convert.ToDateTime(fileReader.ReadLine())
+                            Case 2
+                                numMaxCopies.Value = Convert.ToInt32(fileReader.ReadLine())
+                            Case 3
+                                dtNextBackup = Convert.ToDateTime(fileReader.ReadLine())
+                            Case Else
+                                lstPathsToCopy.Items.Add(fileReader.ReadLine())
+                        End Select
+
+                        intCounter += 1
+                    End While
+                End Using
+            End If
+        Catch ex As Exception
+            ShowErrorMessage(ex)
+        End Try
+    End Sub
+
+    Private Sub WriteSettings()
+        Try
+            If Not IO.Directory.Exists(System.IO.Path.Combine(System.Environment.SpecialFolder.ApplicationData, "WeeklyBackupScheduler")) Then
+                IO.Directory.CreateDirectory(System.IO.Path.Combine(System.Environment.SpecialFolder.ApplicationData, "WeeklyBackupScheduler"))
+            End If
+
+            Using fileWriter As IO.StreamWriter = New IO.StreamWriter(strSettingsPath, False)
+                fileWriter.WriteLine(txtLocation.Text)
+                fileWriter.WriteLine(dtpTime.Value.ToShortTimeString())
+                fileWriter.WriteLine(numMaxCopies.Value)
+                fileWriter.WriteLine(dtNextBackup)
+                For Each item As String In lstPathsToCopy.Items
+                    fileWriter.WriteLine(item)
+                Next
+            End Using
+        Catch ex As Exception
+            ShowErrorMessage(ex)
+        End Try
+    End Sub
+#End Region
+
+End Class
---
+++ BackupSystem/frmMain.Designer.vb
@@ -0,0 +1,241 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.lblLocation = New System.Windows.Forms.Label()
+        Me.lblCopyPaths = New System.Windows.Forms.Label()
+        Me.lblTime = New System.Windows.Forms.Label()
+        Me.lstPathsToCopy = New System.Windows.Forms.ListBox()
+        Me.txtLocation = New System.Windows.Forms.TextBox()
+        Me.cmdBrowseCopy = New System.Windows.Forms.Button()
+        Me.cmdRemove = New System.Windows.Forms.Button()
+        Me.cmdSchedule = New System.Windows.Forms.Button()
+        Me.cmdBrowseLoc = New System.Windows.Forms.Button()
+        Me.dtpTime = New System.Windows.Forms.DateTimePicker()
+        Me.fdbGetFolder = New System.Windows.Forms.FolderBrowserDialog()
+        Me.grpOptions = New System.Windows.Forms.GroupBox()
+        Me.cbBackupNow = New System.Windows.Forms.CheckBox()
+        Me.lblMaxCopies = New System.Windows.Forms.Label()
+        Me.lblCopies = New System.Windows.Forms.Label()
+        Me.numMaxCopies = New System.Windows.Forms.NumericUpDown()
+        Me.icoNotify = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.grpOptions.SuspendLayout()
+        CType(Me.numMaxCopies, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblLocation
+        '
+        Me.lblLocation.AutoSize = True
+        Me.lblLocation.Location = New System.Drawing.Point(26, 18)
+        Me.lblLocation.Name = "lblLocation"
+        Me.lblLocation.Size = New System.Drawing.Size(91, 13)
+        Me.lblLocation.TabIndex = 0
+        Me.lblLocation.Text = "Backup Location:"
+        '
+        'lblCopyPaths
+        '
+        Me.lblCopyPaths.AutoSize = True
+        Me.lblCopyPaths.Location = New System.Drawing.Point(5, 67)
+        Me.lblCopyPaths.Name = "lblCopyPaths"
+        Me.lblCopyPaths.Size = New System.Drawing.Size(112, 13)
+        Me.lblCopyPaths.TabIndex = 1
+        Me.lblCopyPaths.Text = "Paths of Files to Copy:"
+        '
+        'lblTime
+        '
+        Me.lblTime.AutoSize = True
+        Me.lblTime.Location = New System.Drawing.Point(24, 44)
+        Me.lblTime.Name = "lblTime"
+        Me.lblTime.Size = New System.Drawing.Size(93, 13)
+        Me.lblTime.TabIndex = 3
+        Me.lblTime.Text = "Time for Backups:"
+        '
+        'lstPathsToCopy
+        '
+        Me.lstPathsToCopy.FormattingEnabled = True
+        Me.lstPathsToCopy.Location = New System.Drawing.Point(123, 67)
+        Me.lstPathsToCopy.Name = "lstPathsToCopy"
+        Me.lstPathsToCopy.Size = New System.Drawing.Size(366, 95)
+        Me.lstPathsToCopy.Sorted = True
+        Me.lstPathsToCopy.TabIndex = 5
+        '
+        'txtLocation
+        '
+        Me.txtLocation.Location = New System.Drawing.Point(123, 15)
+        Me.txtLocation.Name = "txtLocation"
+        Me.txtLocation.ReadOnly = True
+        Me.txtLocation.Size = New System.Drawing.Size(366, 20)
+        Me.txtLocation.TabIndex = 6
+        '
+        'cmdBrowseCopy
+        '
+        Me.cmdBrowseCopy.Location = New System.Drawing.Point(495, 67)
+        Me.cmdBrowseCopy.Name = "cmdBrowseCopy"
+        Me.cmdBrowseCopy.Size = New System.Drawing.Size(75, 23)
+        Me.cmdBrowseCopy.TabIndex = 7
+        Me.cmdBrowseCopy.Text = "Browse"
+        Me.cmdBrowseCopy.UseVisualStyleBackColor = True
+        '
+        'cmdRemove
+        '
+        Me.cmdRemove.Location = New System.Drawing.Point(495, 139)
+        Me.cmdRemove.Name = "cmdRemove"
+        Me.cmdRemove.Size = New System.Drawing.Size(75, 23)
+        Me.cmdRemove.TabIndex = 8
+        Me.cmdRemove.Text = "Remove"
+        Me.cmdRemove.UseVisualStyleBackColor = True
+        '
+        'cmdSchedule
+        '
+        Me.cmdSchedule.Location = New System.Drawing.Point(224, 229)
+        Me.cmdSchedule.Name = "cmdSchedule"
+        Me.cmdSchedule.Size = New System.Drawing.Size(133, 23)
+        Me.cmdSchedule.TabIndex = 9
+        Me.cmdSchedule.Text = "Schedule"
+        Me.cmdSchedule.UseVisualStyleBackColor = True
+        '
+        'cmdBrowseLoc
+        '
+        Me.cmdBrowseLoc.Location = New System.Drawing.Point(495, 13)
+        Me.cmdBrowseLoc.Name = "cmdBrowseLoc"
+        Me.cmdBrowseLoc.Size = New System.Drawing.Size(75, 23)
+        Me.cmdBrowseLoc.TabIndex = 10
+        Me.cmdBrowseLoc.Text = "Browse"
+        Me.cmdBrowseLoc.UseVisualStyleBackColor = True
+        '
+        'dtpTime
+        '
+        Me.dtpTime.CustomFormat = "hh:mm tt"
+        Me.dtpTime.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.dtpTime.Location = New System.Drawing.Point(123, 41)
+        Me.dtpTime.Name = "dtpTime"
+        Me.dtpTime.ShowUpDown = True
+        Me.dtpTime.Size = New System.Drawing.Size(73, 20)
+        Me.dtpTime.TabIndex = 11
+        Me.dtpTime.Value = New Date(2018, 2, 9, 3, 0, 0, 0)
+        '
+        'grpOptions
+        '
+        Me.grpOptions.Controls.Add(Me.lblMaxCopies)
+        Me.grpOptions.Controls.Add(Me.lblCopies)
+        Me.grpOptions.Controls.Add(Me.numMaxCopies)
+        Me.grpOptions.Controls.Add(Me.cbBackupNow)
+        Me.grpOptions.Location = New System.Drawing.Point(123, 168)
+        Me.grpOptions.Name = "grpOptions"
+        Me.grpOptions.Size = New System.Drawing.Size(366, 55)
+        Me.grpOptions.TabIndex = 17
+        Me.grpOptions.TabStop = False
+        Me.grpOptions.Text = "Options"
+        '
+        'cbBackupNow
+        '
+        Me.cbBackupNow.AutoSize = True
+        Me.cbBackupNow.Checked = True
+        Me.cbBackupNow.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.cbBackupNow.Location = New System.Drawing.Point(210, 22)
+        Me.cbBackupNow.Name = "cbBackupNow"
+        Me.cbBackupNow.Size = New System.Drawing.Size(124, 17)
+        Me.cbBackupNow.TabIndex = 17
+        Me.cbBackupNow.Text = "Begin a backup now"
+        Me.cbBackupNow.UseVisualStyleBackColor = True
+        '
+        'lblMaxCopies
+        '
+        Me.lblMaxCopies.AutoSize = True
+        Me.lblMaxCopies.Location = New System.Drawing.Point(27, 22)
+        Me.lblMaxCopies.Name = "lblMaxCopies"
+        Me.lblMaxCopies.Size = New System.Drawing.Size(59, 13)
+        Me.lblMaxCopies.TabIndex = 22
+        Me.lblMaxCopies.Text = "Keep up to"
+        '
+        'lblCopies
+        '
+        Me.lblCopies.AutoSize = True
+        Me.lblCopies.Location = New System.Drawing.Point(125, 22)
+        Me.lblCopies.Name = "lblCopies"
+        Me.lblCopies.Size = New System.Drawing.Size(38, 13)
+        Me.lblCopies.TabIndex = 24
+        Me.lblCopies.Text = "copies"
+        '
+        'numMaxCopies
+        '
+        Me.numMaxCopies.Location = New System.Drawing.Point(86, 19)
+        Me.numMaxCopies.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.numMaxCopies.Name = "numMaxCopies"
+        Me.numMaxCopies.Size = New System.Drawing.Size(36, 20)
+        Me.numMaxCopies.TabIndex = 23
+        Me.numMaxCopies.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'icoNotify
+        '
+        Me.icoNotify.Icon = CType(resources.GetObject("icoNotify.Icon"), System.Drawing.Icon)
+        Me.icoNotify.Text = "Weekly Backup Scheduler"
+        Me.icoNotify.Visible = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(579, 262)
+        Me.Controls.Add(Me.grpOptions)
+        Me.Controls.Add(Me.dtpTime)
+        Me.Controls.Add(Me.cmdBrowseLoc)
+        Me.Controls.Add(Me.cmdSchedule)
+        Me.Controls.Add(Me.cmdRemove)
+        Me.Controls.Add(Me.cmdBrowseCopy)
+        Me.Controls.Add(Me.txtLocation)
+        Me.Controls.Add(Me.lstPathsToCopy)
+        Me.Controls.Add(Me.lblTime)
+        Me.Controls.Add(Me.lblCopyPaths)
+        Me.Controls.Add(Me.lblLocation)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmMain"
+        Me.Text = "Weekly Backup Scheduler"
+        Me.grpOptions.ResumeLayout(False)
+        Me.grpOptions.PerformLayout()
+        CType(Me.numMaxCopies, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblLocation As Label
+    Friend WithEvents lblCopyPaths As Label
+    Friend WithEvents lblTime As Label
+    Friend WithEvents lstPathsToCopy As ListBox
+    Friend WithEvents txtLocation As TextBox
+    Friend WithEvents cmdBrowseCopy As Button
+    Friend WithEvents cmdRemove As Button
+    Friend WithEvents cmdSchedule As Button
+    Friend WithEvents cmdBrowseLoc As Button
+    Friend WithEvents dtpTime As DateTimePicker
+    Friend WithEvents fdbGetFolder As FolderBrowserDialog
+    Friend WithEvents grpOptions As GroupBox
+    Friend WithEvents cbBackupNow As CheckBox
+    Friend WithEvents lblMaxCopies As Label
+    Friend WithEvents lblCopies As Label
+    Friend WithEvents numMaxCopies As NumericUpDown
+    Friend WithEvents icoNotify As NotifyIcon
+End Class
